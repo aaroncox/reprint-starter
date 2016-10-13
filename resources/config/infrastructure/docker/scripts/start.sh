e7b5e0159a7da3aa8ef5c7b9f4bc66fe691a13f6
Aaron Cox <acox@carbinestudios.com>
2016-10-12 19:28:44 -0700

    No need to update here, create-project takes care of this

--- resources/config/infrastructure/docker/scripts/start.sh
+++ resources/config/infrastructure/docker/scripts/start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Install app dependencies
-composer -d=/var/www/reprint install --no-interaction
-composer -d=/var/www/reprint update --no-interaction
-
 usermod -u 1000 www-data
 echo '* Starting nginx'
 /usr/bin/supervisord -n -c /etc/supervisord.conf
