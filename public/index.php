6ad889bd9f4862bd9bfc38ddef4216f38da320cf
Aaron Cox <aaron.cox@greymass.com>
2016-11-29 17:15:35 -0800

    Fixed debugging logic in the index.php

--- public/index.php
+++ public/index.php
@@ -7,9 +7,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', (getenv('APPLICATION_PATH') ? getenv('APPLICATION_PATH') : __DIR__));
 
-if(APPLICATION_ENV !== 'development') {
-  Debug::enable();
-}
-
 $app = new Reprint\Application();
+if(APPLICATION_ENV == 'development') {
+  Debug::enable();
+  $app['debug'] = true;
+}
 $app->run();
