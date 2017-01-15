725ad90f1f27f446b7408402914732ad6d95c31e
Aaron Cox <aaron.cox@greymass.com>
2017-01-15 14:49:50 -0800

    Run composer install upon start

--- resources/config/infrastructure/docker/scripts/start.sh
+++ resources/config/infrastructure/docker/scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo '* Installing dependencies via composer'
+/usr/bin/composer install -d /var/www/reprint
 usermod -u 1000 www-data
 echo '* Starting nginx'
 /usr/bin/supervisord -n -c /etc/supervisord.conf
