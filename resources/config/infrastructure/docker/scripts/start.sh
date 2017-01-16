50688020130b4e58968a9fa6ded5145b8fda8b90
Aaron Cox <aaron.cox@greymass.com>
2017-01-15 17:42:50 -0800

    Revert "Run composer install upon start"
    This reverts commit 725ad90f1f27f446b7408402914732ad6d95c31e.

--- resources/config/infrastructure/docker/scripts/start.sh
+++ resources/config/infrastructure/docker/scripts/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo '* Installing dependencies via composer'
-/usr/bin/composer install -d /var/www/reprint
 usermod -u 1000 www-data
 echo '* Starting nginx'
 /usr/bin/supervisord -n -c /etc/supervisord.conf
